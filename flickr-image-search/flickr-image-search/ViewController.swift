be01d02564eea1f483e2804078a7db5f9eb30c1a
Mark Dennis Diwa <markdennisdiwa@gmail.com>
2022-01-22 12:13:25 +0800

    Add initial project and linter rules

---
+++ flickr-image-search/flickr-image-search/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flickr-image-search
+//
+//  Created by Mark Dennis Diwa on 1/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
